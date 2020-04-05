dc0371e3fadeac7bd8beedf9cbc15dbf273cb1d2
Steven Deobald <steven@deobald.ca>
2020-04-05 18:18:16 +0530

    Added a small sample API

---
+++ app/controllers/api/topics_controller.rb
@@ -0,0 +1,25 @@
+class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
+
+  # GET /topics
+  # GET /topics.json
+  def index
+    @topics = Topic.all
+  end
+
+  # GET /topics/1
+  # GET /topics/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def topic_params
+      params.require(:topic).permit(:name, :created_at, :updated_at)
+    end
+end
