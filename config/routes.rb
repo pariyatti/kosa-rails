9ca13bcf48bf7977eb4b107caff5e5a11da12f42
Steven Deobald <steven@deobald.ca>
2020-04-05 17:53:51 +0530

    Made a little homepage that lays out the models

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :topics
+  resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'authors#index'
+  root 'application#index'
 end
