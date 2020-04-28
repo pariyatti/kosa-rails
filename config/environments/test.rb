8162d093b653d1457e3e664bb4bfd0a9d6c04617
Steven Deobald <steven@deobald.ca>
2020-04-28 23:17:32 +0530

    adding production env
    - following the recommended environment layout for ansible
    - had to hack and slash the neo4j password reset... really consider a custom role.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -44,8 +44,6 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
-  # TODO: this is the development database... obviously create a test db
-  #       ...but neo4j only permits one db at a time? wat. -sd
   config.neo4j.session.type = :bolt
   config.neo4j.session.path = "bolt://localhost:7006"
 end
