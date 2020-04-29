71d1d485961c63274df66c87a57dc26aa4b2f033
Steven Deobald <steven@deobald.ca>
2020-04-29 16:15:29 +0530

    ssh-fix script needs to talk to our new hosts files

--- deployment/bin/fix-ssh-hangs.sh
+++ deployment/bin/fix-ssh-hangs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 echo testing network access to ansible...
-ansible all -i hosts -u root -m ping
+ansible all -i environments/sandbox -u root -m ping
+ansible all -i environments/production -u root -m ping
 
 echo removing dead ssh connections from ansible...
 rm -r ~/.ansible/cp
