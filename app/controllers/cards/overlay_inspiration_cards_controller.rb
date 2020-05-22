db8bceff1b66622f19c7e7df7196eeec7dd14b8e
Varun Pai <varunpai@fastmail.com>
2020-05-22 17:47:58 +0530

    [Do not revert] Made changes to controllers of all card to add index. Added CSS for 'Page header'. Moved 'create' link for each card type in the page header.

--- app/controllers/cards/overlay_inspiration_cards_controller.rb
+++ app/controllers/cards/overlay_inspiration_cards_controller.rb
@@ -2,6 +2,10 @@ class Cards::OverlayInspirationCardsController < CardsController
   before_action :set_card, only: [:show, :edit, :update, :destroy]
   before_action :require_login
   
+  def index
+    @cards = Card.all_for_editors(:overlay_inspiration)
+  end
+
   def show
   end
 
--- app/controllers/cards/pali_word_cards_controller.rb
+++ app/controllers/cards/pali_word_cards_controller.rb
@@ -2,6 +2,10 @@ class Cards::PaliWordCardsController < CardsController
   before_action :set_card, only: [:show, :edit, :update, :destroy]
   before_action :require_login
   
+  def index
+    @cards = Card.all_for_editors(:pali_word)
+  end
+  
   def show
   end
 
