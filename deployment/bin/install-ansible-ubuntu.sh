734850b0c9d4cfd62f0024f7792325e7b3bedef7
Steven Deobald <steven@deobald.ca>
2020-08-04 19:43:46 +0530

    fix ansible install (python3 is now explicit)

--- deployment/bin/install-ansible-ubuntu.sh
+++ deployment/bin/install-ansible-ubuntu.sh
@@ -1,5 +1,10 @@
 sudo apt update
 sudo apt install software-properties-common
+
 sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
+sudo apt update
+
 sudo apt install ansible
-sudo apt install python-argcomplete
+sudo apt install python3-argcomplete
+
